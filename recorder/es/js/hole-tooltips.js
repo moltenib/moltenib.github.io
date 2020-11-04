8af90c794c072359adf9702f162dadccbb0ab7e7
Bruno <stamby@github.com>
2020-11-04 10:06:55 -0300

    Spanish translation

---
+++ recorder/es/js/hole-tooltips.js
@@ -0,0 +1,12 @@
+const HOLE_TOOLTIPS = {
+    "0": "Pulgar Izquierdo",
+    "1": "Índice Izquierdo",
+    "2": "Mayor Izquierdo",
+    "3": "Anular Izquierdo",
+    "4": "Índice Derecho",
+    "5": "Mayor Derecho",
+    "6a": "Anular Derecho",
+    "6b": "Anular Derecho",
+    "7a": "Meñique Derecho",
+    "7b": "Meñique Derecho"
+}
